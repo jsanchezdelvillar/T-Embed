77b6d883cd2367b7fed6bc1fdd8e778aec09611f
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-06-16 14:25:38 +0200

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define WIFI_SSID      "YourRealSSID"
+#define WIFI_PASSWORD  "YourRealPassword"
+#define MQTT_BROKER    "192.168.1.10"    // or your broker's hostname/IP
+#define MQTT_PORT      1883
