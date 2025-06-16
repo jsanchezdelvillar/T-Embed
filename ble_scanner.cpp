f3bf47c556763b50ff8d12f6d97d06da3b56f23a
jsanchezdelvillar <165960348+jsanchezdelvillar@users.noreply.github.com>
2025-06-16 13:31:58 +0200

    Add files via upload

---
+++ ble_scanner.cpp
@@ -0,0 +1,10 @@
+#include "ble_scanner.h"
+#include <Arduino.h>
+
+void BLEScanner::begin() {
+  // Initialize BLE
+}
+
+void BLEScanner::update() {
+  // Scan BLE devices and forward to MQTTClient
+}
